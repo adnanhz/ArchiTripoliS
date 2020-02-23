1732969834951b3dbaa16bed4936480dad2a9793
Adnan Hamzeh <adnan.37h@gmail.com>
2020-02-23 21:07:02 +0200

    fiX

--- src/views/games/Crosswords.vue
+++ src/views/games/Crosswords.vue
@@ -1,48 +1,14 @@
 <template>
-  <div class="crosswords container-fluid">
-    <div
-      style="display: flex; display: -webkit-flex;; flex-wrap: wrap; justify-content: space-around; align-items: center; -webkit-align-items: center;"
-    >
-    <div style="width: 100%; display: flex; display: -webkit-flex;; justify-content: space-between;">
-        <span
-          style="width: 10%; height: 94px; background-color: white; 
-                    padding: 15px; margin-top: 10px; border: 6px solid #f5f5f5;
-                    font-size: 40px;cursor: pointer;"
-          @click="$router.back()"
-        ><i class="icon ion-md-arrow-round-back"></i></span>
-        <MenuItem style="margin-bottom: 150px; width: 89%; font-size: 40px;" bgcolor="#3f5756" title="Mots croisés" />
-    </div>
-      <div class="game-category mots-croises" data-toggle="modal" data-target="#jeu">
-        <div class="game-category-inner">1</div>
-      </div>
-    </div>
-
-
-<!-- Modal -->
-<div class="modal fade" id="jeu" tabindex="-1">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content" style="height: 100vh">
-      <div class="modal-header">
-          <button type="button" class="btn btn-primary" @click="$router.back()" 
-          style="cursor: pointer; float: left;">
-              Retour
-          </button>
-      </div>
-      <div class="modal-body" style="height: 85vh;">
-        <iframe style="width: 90vw; overflow-y: scroll; height: 85vh;" 
+  <div>
+            <iframe style="width: 90vw; overflow-y: scroll; height: 85vh;" 
         src="https://www.bookwidgets.com/play/MK4MH9">
         </iframe>   
-      </div>
-    </div>
-  </div>
-</div>
   </div>
 </template>
 <script>
 import MenuItem from '@/components/MenuItem';
 export default {
     components: {
-        MenuItem
     }
 }
 </script>
